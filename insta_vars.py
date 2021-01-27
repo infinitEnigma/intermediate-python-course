3ddafa30591aff292975e5805ce19703754825ef
infinitEnigma <yole71@gmail.com>
2021-01-27 01:27:52 +0100

    separate file for vars

---
+++ insta_vars.py
@@ -0,0 +1,18 @@
+# instagram variables
+loginButton = '#loginForm > div > div:nth-child(3)'
+dontRememberPass = '//*[@id="react-root"]/section/main/div/div/div/div/button'
+noNotifications = '/html/body/div[4]/div/div/div/div[3]/button[2]'
+firstPost = '//*[@id="react-root"]/section/main/div/div[1]/div/div[1]/div[2]/div/a/div/div[2]'
+followButton = '/html/body/div[5]/div[2]/div/article/header/div[2]/div[1]/div[2]/button'
+likeButton = '/html/body/div[5]/div[2]/div/article/div[3]/section[1]/span[1]/button/div/span'
+commentButton = '/html/body/div[5]/div[2]/div/article/div[3]/section[1]/span[2]/button/div'
+commentTextArea = '/html/body/div[5]/div[2]/div/article/div[3]/section[3]/div/form/textarea'
+submitButton = '/html/body/div[5]/div[2]/div/article/div[3]/section[3]/div//form/button'
+searchBox = '//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input'
+firstResult = '//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div[4]/div/a[1]/div'
+firstResultsPost = '#react-root > section > main > div > div._2z6nI > article > div:nth-child(1) > div > div:nth-child(1) > div:nth-child(1) > a > div > div._9AhH0'
+
+# your posts (comments) - should be replaced with some ai logic
+commentList = ['Awesome post `(super post - testiranje bota u toku, sorry :tada: :) )`', 'Great post `(super post - testiranje bota u toku, sorry :tada: )`', 'I really like your post `(super post - testiranje bota u toku, sorry :tada: )`', 'The best post today `(super post - testiranje bota u toku, sorry :tada: )`']
+
+
--- instabot.py
+++ instabot.py
@@ -1,20 +1,9 @@
 from selenium import webdriver
 from time import sleep
 from random import randint
+import insta_vars 
+import sys
 
-# instagram variables
-loginButton = '#loginForm > div > div:nth-child(3)'
-dontRememberPass = '//*[@id="react-root"]/section/main/div/div/div/div/button'
-noNotifications = '/html/body/div[4]/div/div/div/div[3]/button[2]'
-firstPost = '//*[@id="react-root"]/section/main/div/div[1]/div/div[1]/div[2]/div/a/div/div[2]'
-followButton = '/html/body/div[5]/div[2]/div/article/header/div[2]/div[1]/div[2]/button'
-likeButton = '/html/body/div[5]/div[2]/div/article/div[3]/section[1]/span[1]/button/div/span'
-commentButton = '/html/body/div[5]/div[2]/div/article/div[3]/section[1]/span[2]/button/div'
-commentTextArea = '/html/body/div[5]/div[2]/div/article/div[3]/section[3]/div/form/textarea'
-submitButton = '/html/body/div[5]/div[2]/div/article/div[3]/section[3]/div//form/button'
-
-# your posts (comments) - should be replaced with some ai logic
-commentList = ['Awesome post `(super post - testiranje bota u toku, sorry :tada:)`', 'Great post `(super post - testiranje bota u toku, sorry :tada:)`', 'I really like your post `(super post - testiranje bota u toku, sorry :tada:)`', 'The best post today `(super post - testiranje bota u toku, sorry :tada:)`']
 
 
 # open chrome
@@ -22,10 +11,10 @@ chromewebdriver = webdriver.Chrome()
 chromewebdriver.get('https://instagram.com')
 sleep(randint(5,10)) # sleep while the page loads - change according to your internet speed !!!
 
-# login is ready - input username and password
 username = chromewebdriver.find_element_by_name('username')
 password = chromewebdriver.find_element_by_name('password')
 
+# login is ready - input username and password
 user = input('input Instagram username: ')
 passw = input('input Instagram password: ')
 username.send_keys(user)
@@ -33,44 +22,73 @@ sleep(1)
 password.send_keys(passw)
 sleep(0.5)
 
-# pass through without save credentials and 
-lbutton = chromewebdriver.find_element_by_css_selector(loginButton)
+# pass through without saving credentials and don't allow notifications
+lbutton = chromewebdriver.find_element_by_css_selector(insta_vars.loginButton)
 lbutton.click()
 sleep(3)
-notnow = chromewebdriver.find_element_by_xpath(dontRememberPass)
+notnow = chromewebdriver.find_element_by_xpath(insta_vars.dontRememberPass)
 notnow.click()
 sleep(3)
-notnow = chromewebdriver.find_element_by_xpath(noNotifications)
+notnow = chromewebdriver.find_element_by_xpath(insta_vars.noNotifications)
 notnow.click()
 sleep(randint(5,10))
 
-# logged in, go to instagram's explore page
-chromewebdriver.get('https://instagram.com/explore/')
-sleep(randint(5,10))
+# logged in, define your actions
+print("do you want random search or specific person?")
+action = input("enter 'r' for random or specific name that you want to search for or 'q' for quit: ")
+if action == 'q': sys.exit("goodbye")
 
-# take first post
-post = chromewebdriver.find_element_by_xpath(firstPost)
-post.click()
-sleep(randint(1,3))
 
-# start the loop for following, liking and posting - change to fit your needs
-while True:
-    for item in range(1,10):
-        sleep(randint(3,7))
-        if chromewebdriver.find_element_by_xpath(followButton).text == 'Follow':
-            chromewebdriver.find_element_by_xpath(followButton).click()
-            sleep(randint(1,4))
-            chromewebdriver.find_element_by_xpath(likeButton).click()
-            sleep(randint(2,5))
-            chromewebdriver.find_element_by_xpath(commentButton).click()
-            sleep(randint(1,3))
-            commentbox = chromewebdriver.find_element_by_xpath(commentTextArea)
-            commentbox.click()
-            commentbox.send_keys(commentList[randint(0,3)])
-            sleep(randint(1,3))
-            chromewebdriver.find_element_by_xpath(submitButton).click()
-            sleep(randint(1,3))
-        # go to the next post
-        chromewebdriver.find_element_by_link_text('Next').click()
-    sleep(10000)
+
+# define the loop for following, liking and posting - change to fit your needs
+def FollowLikePost(who):
+    if who == 'r':
+        chromewebdriver.get('https://instagram.com/explore/')
+        sleep(randint(5,10))
+        # take the first post
+        post = chromewebdriver.find_element_by_xpath(insta_vars.firstPost)
+        post.click()
+        sleep(randint(1,3))
+    else:
+        # find instagram users and like all their posts
+        searchbox = chromewebdriver.find_element_by_xpath(insta_vars.searchBox)
+        searchbox.focus = True
+        searchbox.send_keys(who)
+        sleep(2)
+        chromewebdriver.find_element_by_xpath(insta_vars.firstResult).click()
+        sleep(10)
+        chromewebdriver.find_element_by_css_selector(insta_vars.firstResultsPost).click()
+        sleep(5)
+    while True:
+        for item in range(1,10):
+            sleep(randint(3,7))
+            
+            if chromewebdriver.find_element_by_xpath(insta_vars.followButton).text == 'Follow':
+                chromewebdriver.find_element_by_xpath(insta_vars.followButton).click()
+                sleep(randint(1,4))
+                chromewebdriver.find_element_by_xpath(insta_vars.likeButton).click()
+                sleep(randint(2,5))
+                chromewebdriver.find_element_by_xpath(insta_vars.commentButton).click()
+                sleep(randint(1,3))
+                commentbox = chromewebdriver.find_element_by_xpath(insta_vars.commentTextArea)
+                commentbox.click()
+                commentbox.send_keys(insta_vars.commentList[randint(0,3)])
+                sleep(randint(5,8))
+                chromewebdriver.find_element_by_xpath(insta_vars.submitButton).click()
+                sleep(randint(1,3))
+            # go to the next post
+            try:
+                chromewebdriver.find_element_by_link_text('Next').click()
+            except:
+                action = input("enter 'r' for random or specific name that you want to search for or 'q' for quit: ")
+                if action == 'q': sys.exit("goodbye")
+                else: FollowLikePost(action)
+        sleep(10000)
+
+
+
+#start the infinite loop
+FollowLikePost(action)
+    
+    
 
